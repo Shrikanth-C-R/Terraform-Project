8b3ad7a92e57d267e5eeb81b0a06ea65251b4cb4
Shrikanth-C-R <163306855+Shrikanth-C-R@users.noreply.github.com>
2024-03-29 09:42:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 provider "aws" {
+  AWS_ACCESS_KEY_ID     = credentials('AWS_ACCESS_KEY_ID')
+  AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
   region     = "us-east-1"
 }
 resource "aws_instance" "ec2terrafrom" {
