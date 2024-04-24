86ba9c447264ae358d00f899d8f77183071e0c39
Shrikanth-C-R <163306855+Shrikanth-C-R@users.noreply.github.com>
2024-04-24 14:58:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ provider "aws" {
   region     = "us-east-1"
 }
 resource "aws_instance" "ec2terrafrom" {
-  ami           = "ami-033a1ebf088e56e81"
+  ami           = "ami-0a1179631ec8933d7"
   instance_type = "t2.micro"
-  key_name = "key"
+  key_name = "1-click"
   security_groups = ["Tomcat-Sg"]
   count = 1
   tags ={
-  Name = "my_terraform_htppd"
+  Name = "my_terraform_SERVER"
   }
 }
